d40bd6fbd639eaf8ea662969f51beb902a77cf44
Ahn Yong Woo <bluelama130@gmail.com>
2026-03-17 20:47:45 +0900

    Added solver for nuemann boundary conditions. Neumann condition on the right needs testing

--- library/finite_difference.py
+++ library/finite_difference.py
@@ -5,6 +5,7 @@ class fdsolver:
                  boundary_start=('dirichlet', 0),
                  boundary_end=('dirichlet', 0), 
                  f=None):
+        self.set_boundary(boundary_start, boundary_end)
         self.set_interval(start, end, n)
         if f != None:
             self.f = f
@@ -13,7 +14,6 @@ class fdsolver:
         self.n = n
         self.x, self.h = np.linspace(self.start, self.end, n+1, endpoint=True, retstep=True)
         self.y = np.zeros(n+1, dtype=float)
-        self._build_matrix()
 
         
     def set_interval(self, start, end, n):
@@ -51,6 +51,7 @@ class upp_sovler_d(fdsolver):
 
 
     def solve(self):
+        self._build_matrix()
         self.y[0] = self.boundary_start[1]
         self.y[-1] = self.boundary_end[1]
 
@@ -66,6 +67,152 @@ class upp_sovler_d(fdsolver):
         
         return ret_x, ret_y
     
+class upp_solver_n1(fdsolver):
+    def __init__(
+                 self, start=0, end=1, n=10,
+                 boundary_start=('neumann', 0), 
+                 boundary_end = ('dirichlet', 0),
+                 f=None):
+        super().__init__(start, end, n, boundary_start, boundary_end, f)
         
+    def _build_matrix(self):
+        n, h = self.n, self.h
+        self.u = (1./h**2) * np.ones(n+1)
+        self.d = -(2./h**2) * np.ones(n+1)
+        self.l = (1./h**2) * np.ones(n+1)
+        self.u[0] = 0
+        self.l[-1] = 0
+        if self.boundary_start[0] == 'neumann':
+            self.u[1] = 1/h
+            self.d[0] = - 1/h
+            self.d[-1] = 1.
+            self.l[-2] = 0.
+        #right neumann condition needs to be tested.
+        if self.boundary_end[0] == 'neumann':
+            self.u[1] = 0.
+            self.d[0] = 1.
+            self.d[-1] = 1/h
+            self.l[-2] = -1/h
+
+        self.band = (1,1)
+        self.ab = np.array([self.u, self.d, self.l])
+
+
+    def solve(self):
+        #self.y[0] = self.boundary_start[1]
+        #self.y[-1] = self.boundary_end[1]
+        
+        self._build_matrix()
+
+        vec_f = np.vectorize(self.f)
+        b = vec_f(self.x)
+        b[0] = self.boundary_start[1]
+        b[-1] = self.boundary_end[1]
+        
+        self.y = solve_banded(self.band, self.ab, b)
+
+        ret_x = self.x.copy() 
+        ret_y = self.y.copy()
+        
+        return ret_x, ret_y
+   
+class upp_solver_n2(fdsolver):
+    def __init__(
+                 self, start=0, end=1, n=10,
+                 boundary_start=('neumann', 0), 
+                 boundary_end = ('dirichlet', 0),
+                 f=None):
+        super().__init__(start, end, n, boundary_start, boundary_end, f)
+        
+    def _build_matrix(self):
+        n, h = self.n, self.h
+        self.u = (1./h**2) * np.ones(n+1)
+        self.d = -(2./h**2) * np.ones(n+1)
+        self.l = (1./h**2) * np.ones(n+1)
+        self.u[0] = 0
+        self.l[-1] = 0
+        if self.boundary_start[0] == 'neumann':
+            self.u[1] = 1/h
+            self.d[0] = - 1/h
+            self.d[-1] = 1.
+            self.l[-2] = 0.
+        if self.boundary_end[0] == 'neumann':
+            self.u[1] = 0.
+            self.d[0] = 1.
+            self.d[-1] = 1/h
+            self.l[-2] = -1/h
+        self.band = (1,1)
+        self.ab = np.array([self.u, self.d, self.l])
+
+
+    def solve(self):
+        self._build_matrix()
+        vec_f = np.vectorize(self.f)
+        b = vec_f(self.x)
+        b[0] = self.boundary_start[1]
+        b[-1] = self.boundary_end[1]
+        
+        if self.boundary_start[0] == 'neumann':
+            b[0] += self.h/2 * self.f(self.x[0]) 
+        if self.boundary_end[0] == 'neumann':
+            b[-1] += self.h/2 * self.f(self.x[-1])
+        
+        self.y = solve_banded(self.band, self.ab, b)
+
+        ret_x = self.x.copy() 
+        ret_y = self.y.copy()
+        
+        return ret_x, ret_y
+   
+class upp_solver_n3(fdsolver):
+    def __init__(
+                 self, start=0, end=1, n=10,
+                 boundary_start=('neumann', 0), 
+                 boundary_end = ('dirichlet', 0),
+                 f=None):
+        super().__init__(start, end, n, boundary_start, boundary_end, f)
+        
+    def _build_matrix(self):
+        n, h = self.n, self.h
+        self.p = np.zeros(n+1)
+        self.u = (1./h**2) * np.ones(n+1)
+        self.d = -(2./h**2) * np.ones(n+1)
+        self.l = (1./h**2) * np.ones(n+1)
+        self.u[0] = 0
+        self.l[-1] = 0
+        if self.boundary_start[0] == 'neumann':
+            self.p[2] = -1/(2*h)
+            self.u[1] = 2/h
+            self.d[0] = -3/2 * 1/h
+            self.d[-1] = 1.
+            self.l[-2] = 0.
+            self.band = (1,2)
+            self.ab = np.array([self.p, self.u, self.d, self.l])
+        if self.boundary_end[0] == 'neumann':
+            self.u[1] = 0.
+            self.d[0] = 1.
+            self.d[-1] = -2/3 * 1/h
+            self.l[-2] = 2/h 
+            self.p[-3] = -1/(2*h)
+            self.band = (2,1)
+            self.ab = np.array([self.u, self.d, self.l, self.p])
+
+        
+
+    def solve(self):
+        self._build_matrix()
+        vec_f = np.vectorize(self.f)
+        b = vec_f(self.x)
+        b[0] = self.boundary_start[1]
+        b[-1] = self.boundary_end[1]
+        
+        
+        self.y = solve_banded(self.band, self.ab, b)
+
+        ret_x = self.x.copy() 
+        ret_y = self.y.copy()
+        
+        return ret_x, ret_y
+       
 
         
